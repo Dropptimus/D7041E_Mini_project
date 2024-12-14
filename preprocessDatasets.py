2d13016e02b73b7eeb2e3623f7493622a87cf52a
brittawi <britta.wilde@web.de>
2024-12-14 17:06:10 +0100

    started preprocessing datasets

---
+++ preprocessDatasets.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo, list_available_datasets
+import pandas as pd
+
+def preprocess_adult():
+    # load data
+    adult = fetch_ucirepo(id=2)
+    # load data into dataframe for easier preprocessing
+    df = pd.concat([adult["data"]["features"],adult["data"]["targets"] ],axis=1)
+    # replace wrong labels by correct one
+    df["income"].replace("<=50K.","<=50K",inplace=True)
+    df["income"].replace(">50K.",">50K",inplace=True)
+    # drop education column as education and education-num contain the same data
+    df.drop("education",axis=1,inplace=True)
+    
