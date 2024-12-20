01cd752954cf10100d7341da3ab53800fe1d3fda
Dropptimus <emillundin9@gmail.com>
2024-12-20 15:26:57 +0100

    Train test loop

--- clustering.py
+++ clustering.py
@@ -7,7 +7,12 @@ from sklearn.preprocessing import StandardScaler
 from itertools import product
 from collections import ChainMap
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import seaborn as sns
+from ucimlrepo import fetch_ucirepo, list_available_datasets
 
 def cluster_accuracy(y_true, y_pred):
     """
@@ -36,10 +41,8 @@ def clustering_classification(ClusteringClass, cls_name, params, X_train, y_trai
     Test clustering classification on dataset
     """
     RANDOM_SEED = random_seed
-    # get the amount of clusters
-    n_clusters = len(np.unique(y_train))
+
     params = classification_cv(ClusteringClass, cls_name, params, X_train, y_train, k_folds, random_seed)
-    print(params)
 
     # create k-means classifier
     kmeans = ClusteringClass(random_state=RANDOM_SEED, **params)
@@ -67,9 +70,9 @@ def clustering_classification(ClusteringClass, cls_name, params, X_train, y_trai
     test_clusters = kmeans.predict(X_test)
     y_pred_test = np.array([labels_map[cluster] for cluster in test_clusters])
 
-    train_acc, train_f1, test_acc, test_f1 = metrics_and_plot_cm(cls_name, y_train, y_pred_train, y_test, y_pred_test)
+    train_acc, train_f1, test_acc, test_f1, cm_train, cm_test = metrics_and_plot_cm(cls_name, y_train, y_pred_train, y_test, y_pred_test)
 
-    return train_acc, train_f1, test_acc, test_f1
+    return train_acc, train_f1, test_acc, test_f1, cm_train, cm_test
 
 
 def classification_cv(ClusteringClass, cls_name, params, X_train, y_train, k_folds, random_seed):
@@ -122,9 +125,11 @@ def classification_cv(ClusteringClass, cls_name, params, X_train, y_train, k_fol
             else:
                 labels_map_cv = {}
                 for cluster_cv in np.unique(cluster_labels_cv):
-                    class_label_cv = mode(y_train_cv[cluster_labels_cv == cluster_cv])[0] # selects the most common label for that cluster
+                    # selects the most common label for that cluster
+                    class_label_cv = mode(y_train_cv[cluster_labels_cv == cluster_cv])[0]
                     labels_map_cv[cluster_cv] = class_label_cv # map that label to the cluster
-                y_pred_train_cv = np.array([labels_map_cv[cluster_cv] for cluster_cv in cluster_labels_cv]) # maps cluster to labels
+                # maps cluster to labels
+                y_pred_train_cv = np.array([labels_map_cv[cluster_cv] for cluster_cv in cluster_labels_cv])
                 y_pred_valid = np.array([labels_map_cv[cluster_cv] for cluster_cv in valid_clusters])
                 valid_acc = accuracy_score(y_valid, y_pred_valid)
                 train_acc = accuracy_score(y_train_cv, y_pred_train_cv)
@@ -144,11 +149,11 @@ def classification_cv(ClusteringClass, cls_name, params, X_train, y_train, k_fol
         # print("Cross validation average train accuracy:", avg_train_acc / K_FOLDS)
         # print("Cross validation average validation accuracy:", avg_valid_acc / K_FOLDS)
     
-    print("Best params: ", best_params)
+    print("Cross validation best parameters: ", best_params)
 
     return best_params
 
-def metrics_and_plot_cm(clf_name, y_train, y_pred_train, y_test, y_pred_test):
+def metrics_and_plot_cm(clf_name, y_train, y_pred_train, y_test, y_pred_test, display = False):
     """
     Calculate train and test accuracy and f1-score
     """
@@ -168,16 +173,106 @@ def metrics_and_plot_cm(clf_name, y_train, y_pred_train, y_test, y_pred_test):
     print(f"{clf_name}, Train f1-score = {train_f1}, Test f1-score = {test_f1}")
 
     # Confusion Matrix
-    print("Confusion Matrix for train set")
     cm_train = confusion_matrix(y_train, y_pred_train, labels=labels)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm_train, display_labels=labels)
-    disp.plot()
-    plt.show()
+    if display:
+        disp_train = ConfusionMatrixDisplay(confusion_matrix=cm_train, display_labels=labels)
+        disp_train.plot()
+        print("Confusion Matrix for train set")
+        plt.show()
 
-    print("Confusion Matrix for test set")
+    
     cm_test = confusion_matrix(y_test, y_pred_test, labels=labels)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm_test, display_labels=labels)
-    disp.plot()
-    plt.show()
+    if display:
+        disp_test = ConfusionMatrixDisplay(confusion_matrix=cm_test, display_labels=labels)
+        disp_test.plot()
+        print("Confusion Matrix for test set")
+        plt.show()
 
-    return train_acc, train_f1, test_acc, test_f1
+    return train_acc, train_f1, test_acc, test_f1, cm_train, cm_test
+
+def test_classifier(clf, clf_name, params, X_train, y_train, X_test, y_test, display = False):
+    labels = np.unique(y_train)
+    # https://towardsdatascience.com/gridsearchcv-for-beginners-db48a90114ee
+    pipe = Pipeline([('scaler', StandardScaler()), (clf_name, clf)])
+    scoring = 'accuracy'
+    gs_knn = GridSearchCV(pipe,
+                        param_grid=params,
+                        scoring=scoring,
+                        cv=5)
+
+    # Ravel to convert from (len, 1) shape to (len,), warning from sk-learn
+    y_train = np.ravel(y_train)
+    gs_knn.fit(X_train, y_train)
+    print("Cross validation best parameters:", gs_knn.best_params_)
+    # find best model score
+    y_pred_train = gs_knn.best_estimator_.predict(X_train)
+    y_pred_test = gs_knn.best_estimator_.predict(X_test)
+
+    train_acc, train_f1, test_acc, test_f1, cm_train, cm_test = metrics_and_plot_cm(clf_name, y_train, y_pred_train, y_test, y_pred_test)
+
+    return train_acc, train_f1, test_acc, test_f1, cm_train, cm_test
+
+def write(writer, cls_name, cm, dataset_split, step):
+        """
+        Writes confusion matrix to tensorboard
+        """
+        # For adding cm to tensoboard
+        # https://christianbernecker.medium.com/how-to-create-a-confusion-matrix-with-tensorboard-and-pytorch-3344ad5e7209
+        # https://www.geeksforgeeks.org/how-to-dump-confusion-matrix-using-tensorboard-logger-in-pytorch-lightning/
+        fig, ax = plt.subplots(figsize=(10, 8))
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=ax)
+        ax.set_xlabel("Predicted labels")
+        ax.set_ylabel("True labels")
+        ax.set_title(cls_name)
+        writer.add_figure("Confusion matrix " + dataset_split + " " + cls_name, fig, step)
+        plt.close(fig)
+
+def save_metrics_to_dict(clf_name, 
+                         train_acc, 
+                         train_f1, 
+                         test_acc, 
+                         test_f1, 
+                         train_acc_dict, 
+                         train_f1_dict, 
+                         test_acc_dict, 
+                         test_f1_dict):
+        
+        train_acc_dict[clf_name] = train_acc
+        train_f1_dict[clf_name] = train_f1
+        test_acc_dict[clf_name] = test_acc
+        test_f1_dict[clf_name] = test_f1
+
+        return train_acc_dict, train_f1_dict, test_acc_dict, test_f1_dict
+
+# function for converting categorical features into numerical
+def encode_categorical_features(X, encoder):
+    X = encoder.fit_transform(X)
+    return X
+
+def import_dataset(uci_id, encoder):
+    # get the dataset
+    dataset = fetch_ucirepo(id=uci_id) 
+    # load data into dataframe for easier preprocessing
+    df = pd.concat([dataset["data"]["features"],dataset["data"]["targets"] ],axis=1)
+    # remove nan values
+    df.dropna(inplace=True)
+    
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1:]
+    # encode categorical data only for features not for target itself
+    # https://stackoverflow.com/questions/29803093/check-which-columns-in-dataframe-are-categorical
+    cols = X.columns
+    num_cols = X._get_numeric_data().columns
+    #print(num_cols)
+    categorical_cols = list(set(cols) - set(num_cols))
+    #print(categorical_cols)
+    X.loc[:, categorical_cols] = encode_categorical_features(X[categorical_cols], encoder)
+    
+    # check if encoding has worked
+    # https://stackoverflow.com/questions/26924904/check-if-dataframe-column-is-categorical
+    for c in X.columns:
+        if X[c].dtype.name == "category":
+            print(f"WARNING: Column {c} still has categorical values!")
+            
+    # last column is target
+    return X, y
